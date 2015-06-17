d44e06b0f848d13039e2abb0fc66a22f82eb4162
Shevaun Coker <shevaun.coker@gmail.com>
2015-06-17 15:09:26 +1000

    don't commit compiled packages, they don't work so well. Also remove obsolete package.el file

--- Packages/elpa/css-mode-1.0/css-mode-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; css-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "css-mode" "css-mode.el" (21241 32078 0 0))
-;;; Generated autoloads from css-mode.el
-(add-to-list 'auto-mode-alist '("\\.css\\'" . css-mode))
-
-(autoload 'css-mode "css-mode" "\
-Major mode for editing CSS source code.
-
-Key bindings:
-
-\\{css-mode-map}
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("css-mode-pkg.el") (21241 32078 568485
-;;;;;;  0))
-
-;;;***
-
-(provide 'css-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; css-mode-autoloads.el ends here
--- Packages/elpa/rspec-mode-1.10/rspec-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "rspec-mode" "1.10" "Enhance ruby-mode for RSpec")
