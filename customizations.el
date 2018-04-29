9bb54af03c03d97d293f37bd00d9f66193c1ee09
Shevaun Coker <shevaun.coker@gmail.com>
2018-04-29 13:01:45 +1000

    update ag settings to ignore css/sass files

--- customizations.el
+++ customizations.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ag-arguments (quote ("--smart-case" "--stats")))
+ '(ag-ignore-list (quote ("*.css" "*.sass")))
  '(aquamacs-additional-fontsets nil t)
  '(aquamacs-customization-version-id 308 t)
  '(aquamacs-tool-bar-user-customization nil t)
